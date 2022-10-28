bda4f0709a9e12306f8b1b7f87a924af014e3951
Peter Hoggarth <hoggarth.peter@gmail.com>
2022-10-28 19:41:28 -0400

    Adds item API

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class ItemsController: ControllerBase{
+    private readonly IItemsRepository repository;
+
+    public ItemsController(IItemsRepository repository){
+        this.repository = repository;
+    }
+
+    [HttpGet]
+    public IEnumerable<Item> GetItems()
+    {
+        var items = repository.GetItems();
+        return items;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Item> GetItem(Guid id){
+        var item = repository.GetItem(id);
+        if(item is null){
+            return NotFound();
+        }
+        return Ok(item);
+    }
+
+}
---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,8 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories;
+public interface IItemsRepository
+{
+    Item GetItem(Guid id);
+    IEnumerable<Item> GetItems();
+}
